2c9eaa4d88cd0b096a2f7619eaf0ac899ee304cb
Sean Steel <seanrsteel@gmail.com>
2020-09-15 14:48:18 -0600

    Add food_trucks_that_sell method and helper method and test

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Event
   attr_reader :name, :food_trucks
   def initialize(name)
@@ -12,6 +14,18 @@ class Event
   def food_truck_names
     @food_trucks.map do |truck|
       truck.name
-    end 
+    end
+  end
+
+  def food_trucks_that_sell(item)
+    @food_trucks.find_all do |food_truck|
+      food_trucks_with_item(food_truck, item)
+    end
+  end
+
+  def food_trucks_with_item(food_truck, item)
+    food_truck.inventory.find_all do |_item|
+      _item == item
+    end
   end
 end
