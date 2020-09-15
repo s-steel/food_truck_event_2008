8201b4aacd19222e32ae0b3a671da3ee3f2e8ead
Sean Steel <seanrsteel@gmail.com>
2020-09-15 13:07:31 -0600

    Initialize food truck class

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -6,7 +6,13 @@ require 'pry'
 class FoodTruckTest < Minitest::Test
 
   def test_it_has_attributes
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
 
+    assert_instance_of FoodTruck, food_truck
+    assert_equal "Rocky Mountain Pies", food_truck.name
+
+    expected = {}
+    assert_equal expected, food_truck.inventory
   end
 
-end 
+end
