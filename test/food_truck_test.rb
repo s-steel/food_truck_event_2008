dcc298e3b60688b871b4c2d0626883f740c4ddc2
Sean Steel <seanrsteel@gmail.com>
2020-09-15 13:04:48 -0600

    Add food_truck test file

---
+++ test/food_truck_test.rb
@@ -0,0 +1,12 @@
+require './lib/food_truck'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+class FoodTruckTest < Minitest::Test
+
+  def test_it_has_attributes
+
+  end
+
+end 
